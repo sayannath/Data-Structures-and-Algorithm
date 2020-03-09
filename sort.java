fc3b53000a3ff19264b64852070b26e49c3d04be
sayannath <sayannath235@gmail.com>
2020-03-10 02:17:19 +0530

    Array

---
+++ sort.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class sort
+{
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A[] = new int[10];
+        System.out.print("Enter the elements :: ");
+        for(int i=0; i<10;i++)
+        A[i] = Integer.parseInt(br.readLine());
+        for(int i=0; i<10; i++)
+        System.out.println(A[i]);
+    }
+}
